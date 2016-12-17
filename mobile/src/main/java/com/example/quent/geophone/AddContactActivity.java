af46560b9d6338b2568715f6ca670d429bbec2ba
quentin <quentin.ronzeau@gmail.com>
2016-12-18 00:50:51 +0100

    Accueil simplifié & Implémentation fonctionnelle & Affichage Coordonnées contact via classe Contact

--- mobile/src/main/java/com/example/quent/geophone/AddContactActivity.java
+++ mobile/src/main/java/com/example/quent/geophone/AddContactActivity.java
@@ -4,10 +4,16 @@ import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutCompat;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckedTextView;
 import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.ScrollView;
 import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class AddContactActivity extends AppCompatActivity {
 
@@ -17,6 +23,9 @@ public class AddContactActivity extends AppCompatActivity {
     private String mailContact;
     private String bluetoothContact;
     private String wifiContact;
+
+    private Contact newContact;
+    CheckedTextView descriptionVue = null;
 
 
     @Override
@@ -66,18 +75,58 @@ public class AddContactActivity extends AppCompatActivity {
             @Override
             public void onClick(View arg0) {
 
+                Contact newContact = new Contact();
+
                 Toast.makeText(getApplicationContext(),"ECRAN AJOUT CONTACT: SAUVEGARDE CONTACT",Toast.LENGTH_SHORT).show();
+
                 nomContact = editTextNomContact.getText().toString();
+                newContact.setNomContact(nomContact);
                 System.out.println("COORDONNES\n\n");
-                System.out.println(nomContact+"\n");
+                //System.out.println(nomContact+"\n");
+                System.out.println(newContact.getNomContact()+"\n");
+
                 numContact = editTextNumContact.getText().toString();
-                System.out.println(numContact+"\n");
+                newContact.setTelContact(numContact);
+                //System.out.println(numContact+"\n");
+                System.out.println(newContact.getTelContact()+"\n");
+
                 mailContact = editTextMailContact.getText().toString();
-                System.out.println(mailContact+"\n");
+                newContact.setMailContact(mailContact);
+                //System.out.println(mailContact+"\n");
+                System.out.println(newContact.getMailContact()+"\n");
+
                 bluetoothContact = editTextBluetoothContact.getText().toString();
-                System.out.println(bluetoothContact+"\n");
+                newContact.setBluetoothContact(bluetoothContact);
+                //System.out.println(bluetoothContact+"\n");
+                System.out.println(newContact.getBluetoothContact()+"\n");
+
                 wifiContact = editTextWifiContact.getText().toString();
-                System.out.println(wifiContact+"\n");
+                newContact.setWifiContact(wifiContact);
+                //System.out.println(wifiContact+"\n");
+                System.out.println(newContact.getWifiContact()+"\n");
+
+                /*CheckedTextView detail;
+                int nbContact = 0;
+
+                nbContact++;
+
+
+
+                LinearLayout llay = (LinearLayout)findViewById(R.id.linearLayout1);
+
+
+                for (int i = 0; i < newContact.incNbContact(); i++) {
+                    detail = new CheckedTextView(fragmentView.getContext());
+                    detail.setLayoutParams(new LinearLayoutCompat.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT));
+                    detail.setText(detailsArray[i]);
+                    llay.addView(detail);
+                }
+
+                ScrollView sv = (ScrollView) fragmentView.findViewById(R.id.scrollView1);
+                sv.invalidate();
+                sv.requestLayout();*/
+
+
             }
 
         });
--- mobile/src/main/java/com/example/quent/geophone/Contact.java
+++ mobile/src/main/java/com/example/quent/geophone/Contact.java
@@ -1,20 +1,29 @@
 package com.example.quent.geophone;
 
+import android.view.View;
+import android.widget.AdapterView;
+
 /**
  * Created by quent on 17/12/2016.
  */
 
-public class Contact {
+public class Contact implements AdapterView.OnClickListener{
 
     private String nomContact;
     private String telContact;
     private String mailContact;
     private String bluetoothContact;
     private String wifiContact;
+    private int nbContact;
 
     public String getNomContact()
     {
         return nomContact;
+    }
+
+    public void setNomContact(String name)
+    {
+        this.nomContact = name;
     }
 
     public String getTelContact()
@@ -22,9 +31,20 @@ public class Contact {
         return telContact;
     }
 
+    public void setTelContact(String tel)
+    {
+        this.telContact = tel;
+    }
+
+
     public String getMailContact()
     {
         return mailContact;
+    }
+
+    public void setMailContact(String mail)
+    {
+        this.mailContact = mail;
     }
 
     public String getBluetoothContact()
@@ -32,9 +52,43 @@ public class Contact {
         return bluetoothContact;
     }
 
+    public void setBluetoothContact(String bluetooth)
+    {
+        this.bluetoothContact = bluetooth;
+    }
+
     public String getWifiContact()
     {
         return wifiContact;
     }
 
+    public void setWifiContact(String wifi)
+    {
+        this.wifiContact = wifi;
+    }
+
+    public int getNbContact()
+    {
+        return nbContact;
+    }
+
+    public void setNbContact(int nb)
+    {
+        this.nbContact = nb;
+    }
+
+    public int incNbContact()
+    {
+
+        int nombreContact = getNbContact()+1;
+        this.nbContact = nombreContact;
+        return nombreContact;
+    }
+
+
+
+    @Override
+    public void onClick(View v) {
+
+    }
 }
